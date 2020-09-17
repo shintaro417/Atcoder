eae224872c3e6a0a5b221fd11745e3b7620cb4e5
shintaro417 <0wj65w10325483y@au.com>
2020-09-17 11:52:58 +0900

    Create ABC178A.java

---
+++ ABC178/ABC178A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    if(x == 0){
+      System.out.println(1);
+    }else{
+      System.out.println(0);
+    }
+  }
+}
