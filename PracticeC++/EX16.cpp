f98c94ed375c67aa0d93f9a7ce8c2ac329b174d3
shintaro417 <0wj65w10325483y@au.com>
2020-09-23 11:03:41 +0900

    Create EX16.cpp

---
+++ PracticeC++/EX16.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> data(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> data.at(i);
+  }
+
+  // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+  // ここにプログラムを追記
+  //3.ループを使って書く
+  bool flag = false;
+  
+  for(int i = 0;i < 4;i++){
+      if(data.at(i) == data.at(i + 1)){
+          flag = true;
+      }
+      
+      if(flag){
+          cout << "YES" << endl;
+          return 0;
+      }
+  }
+  
+  cout << "NO" << endl;
+}
