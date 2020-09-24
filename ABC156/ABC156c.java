289035aaa47c2d16013e6e9745fbd5270e38df1b
shintaro417 <0wj65w10325483y@au.com>
2020-09-24 11:25:08 +0900

    Update ABC156c.java

--- ABC156/ABC156c.java
+++ ABC156/ABC156c.java
@@ -14,12 +14,18 @@ public class Main {
         
         Arrays.sort(x);
         
+        //iは集会が開かれる座標(範囲は昇順ソートしたx[0]~x[x.length - 1]まで、つまりx1~xnの間)
         for(int i = x[0]; i <= x[x.length - 1];i++){
+            //集会が開かれる座標iにおける消費体力の総和
             int num = 0;
             for(int j = 0;j < n;j++){
+                //各住人が座標iにたどり着くために消費する体力を足しこむ
                 num += Math.pow(x[j] - i,2);
             }
+            //消費体力の総和を比べて小さいほうをansに代入
             ans = Math.min(ans,num);
+            
+            //以下同じ作業を繰り返して最小の値を求めていく
         }
         
         System.out.println(ans);
