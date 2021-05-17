2c5d7465b4ea9a4b3fa530d5c26fc843c33008ee
nuko1129 <shintaro11417@gmail.com>
2021-05-17 21:11:55 +0900

    Create ABC201_A.java

---
+++ ABC201/ABC201_A.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] a = new int[3];
+
+		for(int i = 0;i < a.length;i++) {
+			a[i]= sc.nextInt();
+		}
+
+		Arrays.sort(a);
+
+		if(a[2] - a[1] == a[1] - a[0]) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+		sc.close();
+	}
+}
